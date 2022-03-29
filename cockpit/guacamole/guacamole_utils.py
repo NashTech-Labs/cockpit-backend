7ea368088904ea091b3506cf53ea9d3f629d61b0
Sachin155 <sachinvd7@gmail.com>
2022-03-29 13:47:42 +0530

    ADDED GUACAMOLE API

---
+++ cockpit/guacamole/guacamole_utils.py
@@ -0,0 +1,62 @@
+from guacapy.client import Guacamole
+from guacapy.templates import *
+from .guacamole_connections import *
+import os 
+
+try:
+    __guacamole_client = Guacamole(
+            os.getenv("GUACAMOLE_HOST","localhost:49154/"),
+            os.getenv("GUACAMOLE_USERNAME","guacadmin"),
+            os.getenv("GUACAMOLE_PASSWORD","guacadmin"),
+            method='http',
+            url_path='guacamole'
+        )
+except Exception as e:
+    print("Error in Guacamole client object creattion {}".format(e))
+
+def _get_formatted_connection_data(connection_json, connection_type='ssh'):
+    """returns formatted dictionary object"""
+    try:
+        if connection_type == 'ssh':
+            return get_ssh_connection(connection_json)
+        else:
+            print("Connection type {} not supported".format(connection_type))
+            return None
+    except Exception as e:
+        print("Error in formatting ssh payload \nError {}".format(e))
+        return None
+
+def get_guacamole_token():
+
+    """Returns the guacamole api token"""
+    try:
+        return __guacamole_client.token
+    except Exception as e:
+        print("Error in getting guacamole api token {}".format(e))
+        return None
+
+def create_guacamole_connection(connections_details):
+    """Creats the connection in guacamole datasource
+    example connections details
+    {
+        'name' : 'something_unique'
+        'hostname' : "0.0.0.0",
+        'port' : '22',
+        'protocol' : 'ssh',
+        'username' : 'ubuntu',
+        'password' : 'trynexttime',
+        'private-key' : ''
+    }
+    """
+    
+    try:
+        payload=_get_formatted_connection_data(connections_details,connection_type=connections_details["protocol"])
+        response=__guacamole_client.add_connection(payload)
+        return response
+    except Exception as e :
+        print("Error in creating connection to guacamole \nError:{}".format(e))
+        return None
+
+
+    
+#data=    { 'name' : 'something_unique''hostname' : "0.0.0.0",'port' : '22''protocol' : 'ssh','username' : 'ubuntu''password' : 'trynexttime','private-key' : ''}
