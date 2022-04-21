c91dc45989e9cdab43052a062e0cae209c852598
Jubair Ahmad <jubair.ahmed@knoldus.com>
2022-04-21 12:28:49 +0530

    added mail.py file and sqs pub and sub function in awsutils.py file

--- cockpit/platforms/views.py
+++ cockpit/platforms/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+import json
 
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+from .mail import email
+
+@csrf_exempt
+def send_email(request):
+    print("content in http request: \n{}".format(request))
+    data =json.loads(request.body.decode("utf-8"))
+    print("content in http request: \n{}".format(data))
+
+    try: 
+        if request.method == 'POST':
+            username=data["username"]
+            gmail=data["gmail"]
+            emailsending = email(username,gmail)
+            
+            return JsonResponse({"message":"email successfully send "})
+
+        return JsonResponse({"message":"Invalid http request"})
+    except Exception as e:
+        print("Error in views--> send_email\n{}".format(e))
+        return JsonResponse({"message":"EMail sending failed"})
