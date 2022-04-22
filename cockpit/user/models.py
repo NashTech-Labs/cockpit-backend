f69970e5ecdb9773d6dc8b1428239466290c6360
Sachin155 <sachinvd7@gmail.com>
2022-04-22 17:07:06 +0530

    updated user models

--- cockpit/user/models.py
+++ cockpit/user/models.py
@@ -6,3 +6,11 @@ class User(models.Model):
     id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=250, blank=False)
     email = models.CharField(max_length=250, blank=False)
+
+    class Meta:
+        db_table = "user"
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.username
