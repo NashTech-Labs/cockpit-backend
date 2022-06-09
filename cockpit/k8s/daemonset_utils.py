1ed167ce87dd6ed6b4a72fe5c5122e08ac125705
knoldus <naincy.kumari@knoldus.com>
2022-06-09 20:41:13 +0530

    added create daemonset function

--- cockpit/k8s/daemonset_utils.py
+++ cockpit/k8s/daemonset_utils.py
@@ -44,3 +44,41 @@ def get_daemonsets(cluster_details,namespace="default",all_namespaces=False):
             data=__format_data_for_daemonset(daemonset_list)
             #print("Daemonset under default namespaces: {}".format(data))
             return data
+
+def create_daemon_set_object():
+    container = client.V1Container(
+        name="ds-redis",
+        image="redis",
+        image_pull_policy="IfNotPresent",
+        ports=[client.V1ContainerPort(container_port=6379)],
+    )
+    # Template
+    template = client.V1PodTemplateSpec(
+        metadata=client.V1ObjectMeta(labels={"app": "redis"}),
+        spec=client.V1PodSpec(containers=[container]))
+    # Spec
+    spec = client.V1DaemonSetSpec(
+        selector=client.V1LabelSelector(
+            match_labels={"app": "redis"}
+        ),
+        template=template)
+    # DaemonSet
+    daemonset = client.V1DaemonSet(
+        api_version="apps/v1",
+        kind="DaemonSet",
+        metadata=client.V1ObjectMeta(name="daemonset-redis5"),
+        spec=spec)
+
+    return daemonset
+
+def create_daemon_set(cluster_details,apps_v1_api, daemon_set_object):
+    # Create the Daemonset in default namespace
+    # You can replace the namespace with you have created
+    client_api=__get_kubernetes_appsv1client(
+            bearer_token=cluster_details["bearer_token"],
+            api_server_endpoint=cluster_details["api_server_endpoint"],
+        )
+    api_response=client_api.create_namespaced_daemon_set(
+        namespace="default", body=daemon_set_object
+    )
+    print("Daemonset created. status='%s'" % str(api_response.status))
