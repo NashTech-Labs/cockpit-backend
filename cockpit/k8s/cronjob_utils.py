5784c576df9306b45d6e7804487ebc99059c025b
yamikarajput546 <yamikarajput.233@gmail.com>
2022-06-09 20:35:26 +0530

    fun added for creating cronjobs

--- cockpit/k8s/cronjob_utils.py
+++ cockpit/k8s/cronjob_utils.py
@@ -1,6 +1,16 @@
 from kubernetes import client
 from kubernetes.client import ApiClient
 
+from time import sleep
+
+import kubernetes.client
+from kubernetes.client.rest import ApiException
+from kubernetes import client, config
+
+
+JOB_NAME = "pi"
+
+#get kuberenetes client for listing cronjobs
 def __get_kubernetes_batchv1client(bearer_token,api_server_endpoint):
     try:
         #corev1api=("pod","node","config_map","secret","service","namespace","resource")
@@ -45,3 +55,61 @@ def get_cronjobs(cluster_details,namespace="default",all_namespaces=False):
             data=__format_data_for_cronjob(cronjob_list)
             #print("cronjob under namespaces {}:{}".format(namespace,data))
             return data
+
+
+#get kuberenetes client for creating cronjobs
+def __get_kubernetes_client(bearer_token,api_server_endpoint):
+    try:
+        configuration = kubernetes.client.Configuration()
+        configuration.host = api_server_endpoint
+        configuration.verify_ssl = False
+        configuration.api_key = {"authorization": "Bearer " + bearer_token}
+        client.Configuration.set_default(configuration)
+        with kubernetes.client.ApiClient(configuration) as api_client:
+    # Create an instance of the API class
+            api_instance1 = kubernetes.client.BatchV1beta1Api(api_client)
+        return api_instance1
+    except Exception as e:
+        print("Error getting kubernetes client \n{}".format(e))
+        return None
+
+def create_cron_job(api_instance,cluster_details,namespace):
+    container = client.V1Container(
+    name="pi",
+    image="perl",
+    command=["perl", "-Mbignum=bpi", "-wle", "print bpi(2000)"])
+    # Create and configure a spec section
+    template = client.V1PodTemplateSpec(
+        metadata=client.V1ObjectMeta(labels={"app": "pi"}),
+        spec=client.V1PodSpec(restart_policy="Never", containers=[container]))
+    # Create the specification of deployment
+    spec = client.V1JobSpec(
+        template=template,
+        backoff_limit=4)  #specify the number of retries before considering a Job as failed
+    # Instantiate the job object
+    job = client.V1Job(
+        api_version="batch/v1",
+        kind="Job",
+        metadata=client.V1ObjectMeta(name=JOB_NAME),
+        spec=spec)
+
+
+    client_api= __get_kubernetes_client(
+            bearer_token=cluster_details["bearer_token"],
+            api_server_endpoint=cluster_details["api_server_endpoint"],
+        )
+
+    api_response = client_api.create_namespaced_cron_job(
+        body=client.V1beta1CronJob(
+            api_version='batch/v1beta1',
+            kind='CronJob',
+            metadata=client.V1ObjectMeta(name='cronjob9'),
+            spec = client.V1CronJobSpec(
+
+                schedule="25 17 8 6 2", #     25(minute)   5(pm)   8(date)   june(month) tuesday(day of the week)
+                job_template=job
+            )
+            
+        ),
+        namespace=namespace)
+    print("cronJob created. status='%s'" % str(api_response.status))
