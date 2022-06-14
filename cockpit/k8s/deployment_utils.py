7b03960080f786fbae4222b02d142f49eb8a0c17
mayuri-dhote <mayuri.dhote@knoldus.com>
2022-06-14 18:13:23 +0530

    added CREATE UPDATE DELETE function in deployment_utils file via YAML

--- cockpit/k8s/deployment_utils.py
+++ cockpit/k8s/deployment_utils.py
@@ -1,7 +1,9 @@
+from sys import path_hooks
 from kubernetes import client
 from kubernetes.client import ApiClient
 import yaml
 from os import path
+from kubernetes.client.rest import ApiException
 
 def __get_kubernetes_appsv1client(bearer_token,api_server_endpoint):
     try:
@@ -130,17 +132,103 @@ def delete_deployment(cluster_details, ns, DEPLOYMENT_NAME):
 
 
 
-#  by using yaml file
+#  create function using yaml file
 
-    # def create_deployment(cluster_details):
-#     client_api= __get_kubernetes_appsv1client(
-#             bearer_token=cluster_details["bearer_token"],
-#             api_server_endpoint=cluster_details["api_server_endpoint"],
-#         )
-#     with open(path.join(path.dirname(__file__), "nginx-deployment.yaml")) as f:
-#         dep = yaml.safe_load(f)
-#         resp = client_api.create_namespaced_deployment(
-#             body=dep, namespace="default")
-#         print("Deployment created. status='%s'" % resp.metadata.name)
+def create_deployment(cluster_details, path_to_deployment):
+    client_api= __get_kubernetes_appsv1client(
+            bearer_token=cluster_details["bearer_token"],
+            api_server_endpoint=cluster_details["api_server_endpoint"],
+        )
+    with open(path.join(path.dirname(__file__), path_to_deployment)) as f:
+        dep = yaml.safe_load(f)
+        resp = client_api.create_namespaced_deployment(
+            body=dep, namespace="default")
+        print("Deployment created. status='%s'" % resp.metadata.name)
+        
+TOKEN = "eyJhbGciOiJSUzI1NiIsImtpZCI6IlpQYVBWbm5Kb1FMN0NFMzR3MnlSbXE1R3hlQ3RfTDVQZWNBQmVmSUpNMFEifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJkZWZhdWx0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6ImRhc2hib2FyZC1jbHVzdGVyLXJvbGUtdG9rZW4tdnJtamsiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoiZGFzaGJvYXJkLWNsdXN0ZXItcm9sZSIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6IjcwZGVlYjU1LWVmNDItNGQxNC05NjM3LThkYWFlZGU4NzYwNSIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDpkZWZhdWx0OmRhc2hib2FyZC1jbHVzdGVyLXJvbGUifQ.kilBAHfo2iRdlILhZPFn0XDfSwz3rHSM15h46B9g4wgzt7EJWkvc1GwVhpmcPNmd4Kj_ePcWILJKtjZTQDknkjyNFzD2NeX3v92-LfEA1C5svljwBXFY3S4AUZCfWFhpYEV-qYGsTKqVgeWqGFpy9r8Sd4qkR_zecvAUdF_bRkDJZ7_JDp6BCqvoi_KDmyWxpeDEe6ueYa0KLAHdJMKcH3fmN4h9WnBuuEBg2Q0gEetxdui0cWbRWQQplhR7lP_-V364NmgJByesuT69VSL2CY_DGxb8XU4oFnChlNsHd_Qt3GMRUGrW3DxtHZrdtv34REBV_tR5UqtnVLQCAQpQJA"
+API = "https://34.69.172.158"
+cluster = {
+    "bearer_token" : TOKEN,
+    "api_server_endpoint" : API
+}
 
-# create_deployment(cluster_details)
+create_deployment(cluster, "/home/knoldus/Desktop/RBAC/nginx-deployment.yaml")
+
+
+
+# update function using yaml file
+
+def update_deployment(cluster_details, path_to_deployment):
+    client_api= __get_kubernetes_appsv1client(
+            bearer_token=cluster_details["bearer_token"],
+            api_server_endpoint=cluster_details["api_server_endpoint"],
+        )
+    with open(path.join(path.dirname(__file__), path_to_deployment)) as f:
+        dep = yaml.safe_load(f)
+        resp = client_api.patch_namespaced_deployment(name=nginx, body=dep, namespace="default")
+        print("Deployment updated. status='%s'" % resp.metadata.name)
+        
+
+
+        # Create, update, delete function for deployment yaml.
+
+def create_deployment(cluster_details,yaml_body=None,namespace="default"):
+    try:
+        client_api= __get_kubernetes_corev1client(
+                bearer_token=cluster_details["bearer_token"],
+                api_server_endpoint=cluster_details["api_server_endpoint"],
+            )
+
+    
+        resp = client_api.create_namespaced_deployment(body=yaml_body,
+            namespace="{}".format(namespace))
+        data=__format_data_for_deployment(resp)
+        print("DATA:{}".format(data))
+        print("TYPE:{}".format(type(data)))
+    except ApiException as e:
+        print("ERROR IN create_deployment:\n{}".format(e.body))
+        print("TYPE:{}".format(type(e)))
+
+
+def update_deployment(cluster_details,yaml_body=None,namespace="default"):
+    try:
+        client_api= __get_kubernetes_corev1client(
+                bearer_token=cluster_details["bearer_token"],
+                api_server_endpoint=cluster_details["api_server_endpoint"],
+            )
+        resp = client_api.patch_namespaced_deployment(
+            body=yaml_body,
+            namespace="{}".format(namespace))
+        data=__format_data_for_deployment(resp)
+        print("DATA:{}".format(data))
+        print("TYPE:{}".format(type(data)))
+    except ApiException as e:
+        print("ERROR IN update_deployment:\n{}".format(e.body))
+        print("TYPE:{}".format(type(e)))
+
+
+def delete_deployment(cluster_details,deployment_name,namespace="default"):
+    try:
+        client_api= __get_kubernetes_corev1client(
+                bearer_token=cluster_details["bearer_token"],
+                api_server_endpoint=cluster_details["api_server_endpoint"],
+            )
+        resp=client_api.delete_namespaced_deployment(
+            name=deployment_name, namespace="{}".format(namespace))
+        print("RESPONSE:{}".format(resp))
+        print("TYPE:{}".format(type(resp)))
+    except ApiException as e:
+        print("ERROR IN delete_deployment:\n{}".format(e.body))
+        print("TYPE:{}".format(e))
+        
+
+TOKEN = "eyJhbGciOiJSUzI1NiIsImtpZCI6IlpQYVBWbm5Kb1FMN0NFMzR3MnlSbXE1R3hlQ3RfTDVQZWNBQmVmSUpNMFEifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJkZWZhdWx0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6ImRhc2hib2FyZC1jbHVzdGVyLXJvbGUtdG9rZW4tdnJtamsiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoiZGFzaGJvYXJkLWNsdXN0ZXItcm9sZSIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6IjcwZGVlYjU1LWVmNDItNGQxNC05NjM3LThkYWFlZGU4NzYwNSIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDpkZWZhdWx0OmRhc2hib2FyZC1jbHVzdGVyLXJvbGUifQ.kilBAHfo2iRdlILhZPFn0XDfSwz3rHSM15h46B9g4wgzt7EJWkvc1GwVhpmcPNmd4Kj_ePcWILJKtjZTQDknkjyNFzD2NeX3v92-LfEA1C5svljwBXFY3S4AUZCfWFhpYEV-qYGsTKqVgeWqGFpy9r8Sd4qkR_zecvAUdF_bRkDJZ7_JDp6BCqvoi_KDmyWxpeDEe6ueYa0KLAHdJMKcH3fmN4h9WnBuuEBg2Q0gEetxdui0cWbRWQQplhR7lP_-V364NmgJByesuT69VSL2CY_DGxb8XU4oFnChlNsHd_Qt3GMRUGrW3DxtHZrdtv34REBV_tR5UqtnVLQCAQpQJA"
+API = "https://34.69.172.158"
+cluster = {
+    "bearer_token" : TOKEN,
+    "api_server_endpoint" : API
+}
+cm={
+    "name": "abcd"
+}
+delete_deployment(cluster, deployment_name="nginx")
