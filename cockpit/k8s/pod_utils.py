d2e5aefe579f65f2c67bbeeedc4b78ddf6652521
Jubair Ahmad <jubair.ahmed@knoldus.com>
2022-06-15 09:51:51 +0530

    added utils function for crud operation

--- cockpit/k8s/pod_utils.py
+++ cockpit/k8s/pod_utils.py
@@ -1,8 +1,9 @@
 from os import path
 from kubernetes import client
 from kubernetes.client import ApiClient
+from sympy import re
 import yaml
-
+from kubernetes.client.rest import ApiException 
 
 def __get_kubernetes_corev1client(bearer_token,api_server_endpoint):
     try:
@@ -49,6 +50,61 @@ def get_pods(cluster_details,namespace="default",all_namespaces=False):
             #print("Pods under namespaces {}: {}".format(namespace,data))
             return data
 
+# pod creation with yaml 
+
+
+def create_pod(cluster_details,yaml_body=None,namespace="default"):
+    try:
+        client_api= __get_kubernetes_corev1client(
+                bearer_token=cluster_details["bearer_token"],
+                api_server_endpoint=cluster_details["api_server_endpoint"],
+            )
+
+    
+        resp = client_api.create_namespaced_pod(body=yaml_body,
+            namespace="{}".format(namespace))
+        data=__format_data_for_pod(resp)
+        print("DATA:{}".format(data))
+        print("TYPE:{}".format(type(data)))
+    except ApiException as e:
+        print("ERROR IN create_pod:\n{}".format(e.body))
+        print("TYPE:{}".format(type(e)))
+
+
+def delete_pod(cluster_details,pod_name,namespace="default"):
+    try:
+        client_api= __get_kubernetes_corev1client(
+                bearer_token=cluster_details["bearer_token"],
+                api_server_endpoint=cluster_details["api_server_endpoint"],
+            )
+        resp=client_api.delete_namespaced_pod(
+            name=pod_name, namespace="{}".format(namespace))
+        print("RESPONSE:{}".format(resp))
+        print("TYPE:{}".format(type(resp)))
+    except ApiException as e:
+        print("ERROR IN delete_pod:\n{}".format(e.body))
+        print("TYPE:{}".format(e))
+
+def update_pod(cluster_details,yaml_body=None,namespace="default"):
+    try:
+        client_api= __get_kubernetes_corev1client(
+                bearer_token=cluster_details["bearer_token"],
+                api_server_endpoint=cluster_details["api_server_endpoint"],
+            )
+        resp = client_api.patch_namespaced_pod(
+            body=yaml_body,
+            namespace="{}".format(namespace))
+        data=__format_data_for_pod(resp)
+        print("DATA:{}".format(data))
+        print("TYPE:{}".format(type(data)))
+    except ApiException as e:
+        print("ERROR IN update_pod:\n{}".format(e.body))
+        print("TYPE:{}".format(type(e)))
+
+
+# user need to provide .yaml manifest file to create pod. as well as namespace.
+
+
 
 # pod creation with generated manifest using k8s client
 
@@ -61,60 +117,39 @@ def get_pods(cluster_details,namespace="default",all_namespaces=False):
  
 #     }
 
-def create_pod(cluster_details,pod_details):
-        client_api= __get_kubernetes_corev1client(
-             bearer_token=cluster_details["bearer_token"],
-             api_server_endpoint=cluster_details["api_server_endpoint"],
-         )
-        container = client.V1Container(
-            name=pod_details["pod_name"],
-            image=pod_details["image"],
-            # command=['bash', '-c'],
-            # args=[client_api.test_command],
-            image_pull_policy='Always',
-            # working_dir=client_api.working_dir,
-            stdin=True,
-            tty=True
-        )
-        pod_spec = client.V1PodSpec(
-            containers=[container],
-            restart_policy='Never'
-        )
-        pod = client.V1Pod(
-            api_version="v1",
-            kind="Pod",
-            metadata=client.V1ObjectMeta(name=pod_details["pod_name"]),
-            spec=pod_spec
-        )
-
-        try:
-            resp=client_api.create_namespaced_pod(pod_details["namespace"], pod)
-            print("pod created. status='%s'" % resp.metadata.name)
-        except client.rest.ApiException as e:
-            print("Got exception: {} while creating a pod".format(e))
-            return 1
-
-        return 0 
-
-
-
-
-# pod creation with yaml 
-
-
-# def create_pod(cluster_details,namespace="default"):
-#     try:
+# def create_pod(cluster_details,pod_details):
 #         client_api= __get_kubernetes_corev1client(
-#                 bearer_token=cluster_details["bearer_token"],
-#                 api_server_endpoint=cluster_details["api_server_endpoint"],
-#             )
+#              bearer_token=cluster_details["bearer_token"],
+#              api_server_endpoint=cluster_details["api_server_endpoint"],
+#          )
+#         container = client.V1Container(
+#             name=pod_details["pod_name"],
+#             image=pod_details["image"],
+#             # command=['bash', '-c'],
+#             # args=[client_api.test_command],
+#             image_pull_policy='Always',
+#             # working_dir=client_api.working_dir,
+#             stdin=True,
+#             tty=True
+#         )
+#         pod_spec = client.V1PodSpec(
+#             containers=[container],
+#             restart_policy='Never'
+#         )
+#         pod = client.V1Pod(
+#             api_version="v1",
+#             kind="Pod",
+#             metadata=client.V1ObjectMeta(name=pod_details["pod_name"]),
+#             spec=pod_spec
+#         )
 
-#         with open(path.join(path.dirname(__file__), "pod.yaml")) as f:
-#             pod_yaml = yaml.safe_load(f)
-#             resp = client_api.create_namespaced_pod(
-#                 namespace ,pod_yaml)
+#         try:
+#             resp=client_api.create_namespaced_pod(pod_details["namespace"], pod)
 #             print("pod created. status='%s'" % resp.metadata.name)
-#     except Exception as e:
-#         print("Error creating pod \n{}".format(e))
+#         except client.rest.ApiException as e:
+#             print("Got exception: {} while creating a pod".format(e))
+#             return 1
 
-    ## user need to provide .yaml manifest file to create pod. as well as namespace.
+#         return 0 
+
+
