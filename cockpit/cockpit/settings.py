17aebbd7c2f4e8d5656d7c93ef4a96b2d5195d0e
Sachin155 <sachinvd7@gmail.com>
2022-06-15 14:20:02 +0530

    CORS UPDATE

--- cockpit/cockpit/settings.py
+++ cockpit/cockpit/settings.py
@@ -61,10 +61,10 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
 # CORS_ALLOWED_ORIGINS = [
-#     'http://localhost:3030',
+#     'http://localhost:4200',
 # ] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
 # CORS_ALLOWED_ORIGIN_REGEXES = [
-#     'http://localhost:3030',
+#     'http://localhost:4200',
 # ]
 
 ROOT_URLCONF = 'cockpit.urls'
